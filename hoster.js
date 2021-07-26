09a08a1a411ecb98e06bd64801452027a6c3f174
Francis John Clavano <clavmall.85@gmail.com>
2021-07-26 16:49:48 +0800

    Editing winston

--- hoster.js
+++ hoster.js
@@ -1,11 +1,5 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-
-fs.appendFile("logfile.log", "Hello content!", function (err) {
-  if (err) throw err;
-  console.log("Saved!");
-});
 
 require("./startup/logging");
 require("./startup/config");
