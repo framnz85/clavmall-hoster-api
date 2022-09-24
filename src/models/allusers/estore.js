2c60fcc89224a098568d4dacc281e70ce9247015
framnz85 <clavmall.85@gmail.com>
2022-09-24 17:42:47 +0800

    commit 9-24-22 8:00 pm

--- src/models/allusers/estore.js
+++ src/models/allusers/estore.js
@@ -1,5 +1,6 @@
 const conn = require("../../db/allusers");
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 const Joi = require("joi");
 
 const estoreSchema = new mongoose.Schema({
@@ -17,6 +18,11 @@ const estoreSchema = new mongoose.Schema({
     minlength: 5,
     maxlength: 255,
   },
+  country: {
+    type: ObjectId,
+    required: true,
+    default: "60e865ce9333b731c74b232f",
+  },
   email: String,
   mobile: String,
   password: String,
@@ -26,6 +32,34 @@ const estoreSchema = new mongoose.Schema({
   },
   urlname2: String,
   urlname3: String,
+  showHomeCarousel: {
+    type: Boolean,
+    default: true,
+  },
+  showRandomItems: {
+    type: Boolean,
+    default: true,
+  },
+  showCategories: {
+    type: Boolean,
+    default: true,
+  },
+  showNewArrival: {
+    type: Boolean,
+    default: true,
+  },
+  showBestSeller: {
+    type: Boolean,
+    default: true,
+  },
+  headerColor: {
+    type: String,
+    default: "#009A57",
+  },
+  carouselColor: {
+    type: String,
+    default: "",
+  },
   status: {
     type: String,
     default: "pending",
@@ -36,10 +70,67 @@ const estoreSchema = new mongoose.Schema({
       "active",
     ],
   },
-  estoreName: String,
-  estoreEmail: String,
-  estoreSupid: String,
-  estoreUrlname: String,
+  categoryChange: {
+    type: Number,
+    default: 0,
+  },
+  subcatChange: {
+    type: Number,
+    default: 0,
+  },
+  parentChange: {
+    type: Number,
+    default: 0,
+  },
+  productChange: {
+    type: Number,
+    default: 0,
+  },
+  locationChange: {
+    type: Number,
+    default: 0,
+  },
+  estoreChange: {
+    type: Number,
+    default: 0,
+  },
+  imageStorage: {
+    type: String,
+    enum: [
+      "clavmall",
+      "cloudinary"
+    ],
+  },
+  planType: {
+    type: String,
+    default: "plan-1",
+    enum: [
+      "plan-1",
+      "plan-2",
+      "plan-3"
+    ]
+  },
+  endDate: Date,
+  recurringCycle: {
+    type: String,
+    default: "One",
+    enum: [
+      "One",
+      "Unlimited"
+    ]
+  },
+  billingHistory: [
+    {
+      cycleId: String,
+      cycleType: String,
+      totalPrice: String,
+      payment: String,
+      payStatus: String,
+      duration: Number,
+      planId: String,
+      subscriptionID: String,
+    },
+  ],
 });
 
 const Estore = conn.model("Estore", estoreSchema);
